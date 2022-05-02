08887dace433e0e2cbe66306ebec157b2ee76bee
EstherRH00 <eruanohortoneda@gmail.com>
2022-05-02 18:54:28 +0200

    primera iteracio visualitzar malta

---
+++ assets/js/maletaRapida.js
@@ -0,0 +1,12 @@
+const maletaRapida = Vue.createApp({
+    data: function(){
+        return {
+            desti: "",
+            anada: "",
+            tornada: ""
+        };
+    },
+    methods: {
+    }
+});
+maletaRapida.mount("#maletaRapida");
