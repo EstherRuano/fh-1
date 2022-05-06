05f9230e3224c3d43a86da163e842b46d56a6a0a
pbaldisa <69910092+pbaldisa@users.noreply.github.com>
2022-05-06 20:56:56 +0200

    opcions esquerra estandaritzades viz maleta

--- assets/js/visualitzar_maleta.js
+++ assets/js/visualitzar_maleta.js
@@ -3,15 +3,37 @@ const viz_maleta = Vue.createApp({
         return {
             hideCard: true,
             selected: false,
+            recentSelected: false,
+            favoritsSelected: false,
+            papereraSelected:false
         };
     },
     methods: {
         hideNewOutfit(hide) {
-            console.log(hide);
             this.hideCard = !(!hide && this.selected);
         },
         selectNewOutfit() {
             this.selected = !this.selected;
+        },
+        selectedFolder(opcio){
+            if ( opcio == 1 ){
+                console.log("executant opcio 1");
+                this.recentSelected = !this.recentSelected;
+                this.favoritsSelected = false;
+                this.papereraSelected = false;
+            }
+            else if (opcio == 2){
+                console.log("executant opcio 2");
+                this.recentSelected = false;
+                this.favoritsSelected = !this.favoritsSelected;
+                this.papereraSelected = false;
+            }
+            else if (opcio == 3){
+                console.log("executant opcio 3");
+                this.recentSelected = false;
+                this.favoritsSelected = false;
+                this.papereraSelected = !this.papereraSelected;
+            }
         }
         
     }
