6b2e186c5905beef18ca3c10d6dc761a96190ffb
pbaldisa <69910092+pbaldisa@users.noreply.github.com>
2022-05-07 00:06:03 +0200

    Success alert on adding outfit to suitcase

--- assets/js/visualitzar_maleta.js
+++ assets/js/visualitzar_maleta.js
@@ -9,12 +9,35 @@ const viz_maleta = Vue.createApp({
         };
     },
     methods: {
+        //s'executa al desar i al cance·lar, segons hide (cancel -> hide = true i anàleg per desa)
         hideNewOutfit(hide) {
             this.hideCard = !(!hide && this.selected);
+            //treu la selecció del outfit tant si cancel·la com si desa
+            this.selected = false;
         },
+        //s'executa quan es clica sobre un outfit per seleccionar-lo
         selectNewOutfit() {
             this.selected = !this.selected;
         },
+        //mostra alerta
+        showAlert() {
+            //mostra que ha estat un èxit només quan la card de l'outfit esta per ensenyar
+            if(!this.hideCard){
+                var container = document.querySelector("#alert-container");
+                container.innerHTML = `<div id="succeed-alert" class="alert alert-success alert-dismissible" role="alert">
+                <strong>Èxit!</strong> Outfit afegit correctament a la maleta.
+                </div>`;
+                const alert = document.querySelector("#succeed-alert");
+                // Create Bootstrap alert instance
+                const bsAlert = new bootstrap.Alert(alert);
+
+                // Dismiss time out
+                setTimeout(() => {
+                bsAlert.close();
+                }, 2000);
+            }
+        },
+        //lògica de pestanyes
         selectedFolder(opcio){
             if ( opcio == 1 ){
                 console.log("executant opcio 1");
