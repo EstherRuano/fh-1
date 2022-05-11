d919c18c0b52c43ac0a24ab3152357cb1e9c6257
EstherRH00 <eruanohortoneda@gmail.com>
2022-05-12 00:01:06 +0200

    prova data

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,45 @@
+$(function() {
+    $('input[name="daterange"]').daterangepicker({
+        "autoApply": true,
+    "locale": {
+        "format": "DD/MM/YYYY",
+        "separator": " - ",
+        "applyLabel": "Apply",
+        "cancelLabel": "Cancel",
+        "fromLabel": "From",
+        "toLabel": "To",
+        "customRangeLabel": "Custom",
+        "weekLabel": "W",
+        "daysOfWeek": [
+            "Dg",
+            "Dl",
+            "Dt",
+            "Dc",
+            "Dj",
+            "Dv",
+            "Ds"
+        ],
+        "monthNames": [
+            "Gener",
+            "Febrer",
+            "Març",
+            "Abril",
+            "Maig",
+            "Juny",
+            "Juliol",
+            "Agost",
+            "Setembre",
+            "Octubre",
+            "Novembre",
+            "Desembre"
+        ],
+        "firstDay": 1
+    }
+}, function(start, end, label) {
+  console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')');
+})});
+
+
 const index = Vue.createApp({
     data: function(){
         return {
@@ -8,8 +50,14 @@ const index = Vue.createApp({
     },
     methods: {
         envia(){
-            location.href=('maleta_rapida_NoLogin.html?desti=').concat(this.desti).concat('&anada=').concat(this.anada).concat('&tornada=').concat(this.tornada);
-        }
+            a = document.getElementById("anada").value;
+            location.href=('maleta_rapida_NoLogin.html?desti=').concat(this.desti).concat('&anada=').concat(a).concat('&tornada=').concat(this.tornada);
+        },
+        setTexto(event){ //event se pasa automáticamente sp que se genera un evento
+            console.log("Ejecutando setTexto()...");
+            this.texto = event.target.value; //r-value es vanilla JS
+          },
+
     }
 });
 index.mount("#index");
